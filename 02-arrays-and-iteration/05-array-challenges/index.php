44bf5a557e39e753297674268e365970e6bdc5fd
Josh Harris <49429928+jharris711@users.noreply.github.com>
2024-08-22 21:41:03 -0400

    challenges

---
+++ 02-arrays-and-iteration/05-array-challenges/index.php
@@ -0,0 +1,85 @@
+<?php
+
+/*
+  Challenge 1: Sum of an array
+  
+  1. Create an array of numbers 
+  2. Get the sum of all of the numbers combined and put into a variable.
+  4. Get the amount of numbers in the array and put into a variable.
+  5. Print out 'The sum of the {amount} numbers is: {sum} '. For example, if the array is [1, 2, 3, 4, 5], the output should be 'The sum of the 5 numbers is: 15'. 
+*/
+echo '<h3>Sum of an Array</h3>';
+$nums = [1, 2, 3, 4, 5];
+$sum = array_sum($nums);
+$length = count($nums);
+echo '<h3>The sum of the ' . $length . ' numbers is: ' . $sum . '</h3>';
+
+/*
+  Challenge 2: Colors array
+
+  1. Reverse the `$colors` array.
+  2. Add 'purple' and 'orange' to the end of the array.
+  3. Replace the second color with 'pink'
+  4. Remove the last element of the array.
+
+You should end up with the following array: ['yellow', 'pink', 'blue', 'red', 'purple']
+*/
+
+echo '<h3>Colors Array</h3>';
+
+$colors = ['red', 'blue', 'green', 'yellow'];
+$colors = array_reverse($colors);
+array_push($colors, 'purple', 'orange');
+array_pop($colors);
+
+var_dump($colors);
+
+/*
+  Challenge 3: Job listings array
+
+  1. Create a multi-dimensional array of associative arrays of 3 job listings with the fields id, job_title, company, contact_email, and contact_phone. Also add an array field for skills. The skills array should be an array of strings with each skill a person has. For example, 'PHP', 'MySQL', 'JavaScript', 'HTML', 'CSS', etc.
+  2. Create a new listing using the `array_push()` function. The new listing should have the same fields as the others.
+  3. Print out the job_title of the second job listing in the array.
+  4. Print out the first skill of the third job listing in the array.
+*/
+
+echo '<h3>Job Listings</h3>';
+
+$jobListings = [
+  [
+    'id' => 0,
+    'job_title' => 'Web Developer',
+    'company' => 'ABC Inc.',
+    'contact_email' => 'admin@gmail.com',
+    'contact_phone' => '123-456-7890',
+    'skills' => ['PHP', 'MySQL', 'JavaScript', 'HTML', 'CSS'],
+  ],
+  [
+    'id' => 1,
+    'job_title' => 'Gardener',
+    'company' => 'Garden Inc.',
+    'contact_email' => 'gardenboss@gmail.com',
+    'contact_phone' => '123-456-7890',
+    'skills' => ['Digging', 'Plant stuff', 'Dirt'],
+  ],
+  [
+    'id' => 2,
+    'job_title' => 'Jobless',
+    'company' => 'NA',
+    'contact_email' => 'lol',
+    'contact_phone' => '123-456-7890',
+    'skills' => ['Gaming', 'Procrastination', 'Lazing about'],
+  ],
+];
+
+array_push($jobListings, [
+  'id' => 0,
+  'job_title' => 'Roblox Millionaire',
+  'company' => 'Acme Inc.',
+  'contact_email' => 'me@gmail.com',
+  'contact_phone' => '123-456-7890',
+  'skills' => ['Lua', 'Roblox Studio', 'Robux'],
+]);
+
+echo '<h3>Job Title of Second Job Listing: ' . $jobListings[1]['job_title'] . '</h3>';
+echo '<h3>First Skill of Third Job Listing: ' . $jobListings[2]['skills'][0] . '</h3>';
