c6f51ecf3ac38fd405febbe1e919837aa3e42617
Josh Harris <49429928+jharris711@users.noreply.github.com>
2024-08-22 21:56:50 -0400

    basic loops

---
+++ 02-arrays-and-iteration/06-basic-loops/index.php
@@ -0,0 +1,63 @@
+<?php
+
+/* for ($i = 0; $i < 10; $i++) {
+    echo $i . '<br>';
+} */
+
+/* $i = 0;
+while ($i < 10) {
+    echo $i . '<br>';
+    $i++;
+}
+ */
+
+
+/* $i = 0;
+do {
+    echo $i . '<br>';
+    $i++;
+} while ($i < 10); */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>PHP From Scratch</title>
+</head>
+
+<body class="bg-gray-100">
+    <header class="bg-blue-500 text-white p-4">
+        <div class="container mx-auto">
+            <h1 class="text-3xl font-semibold">PHP From Scratch</h1>
+        </div>
+    </header>
+    <div class="container mx-auto p-4 mt-4">
+        <div class="bg-white rounded-lg shadow-md p-6 mt-6">
+            <ul>
+                <?php for ($i = 0; $i <= 10; $i++) : ?>
+                    <li>Number: <?= $i ?></li>
+                <?php endfor; ?>
+            </ul>
+            <ul>
+                <?php $i = 0;
+                while ($i < 10) : ?>
+                    <li>Number: <?= $i ?></li>
+                <?php $i++;;
+                endwhile; ?>
+            </ul>
+            <ul>
+                <?php $i = 0;
+                do { ?>
+                    <li>Number: <?= $i ?></li>
+                <?php $i++;
+                } while ($i < 10); ?>
+            </ul>
+        </div>
+    </div>
+</body>
+
+</html>
