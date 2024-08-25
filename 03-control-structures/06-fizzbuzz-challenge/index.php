739605028fa69299550f664fceda10f442b422eb
Josh Harris <49429928+jharris711@users.noreply.github.com>
2024-08-25 19:56:36 -0400

    fizzbuzz

---
+++ 03-control-structures/06-fizzbuzz-challenge/index.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Fizzbuzz Challenge:
+Write a program that prints the numbers from 1 to 100. 
+But for multiples of three print “Fizz” instead of 
+the number and for the multiples of five print “Buzz”. 
+For numbers which are multiples of both three and five 
+print “FizzBuzz”. Remember, you can use the modulus 
+operator to check if a number is divisible by another 
+number.
+*/
+
+echo '<div>';
+for ($i = 1; $i <= 100; $i++) {
+    if ($i % 3 === 0 && $i % 5 === 0) {
+        echo 'FizzBuzz<br>';
+    } elseif ($i % 3 === 0) {
+        echo 'Fizz<br>';
+    } elseif ($i % 5 === 0) {
+        echo 'Buzz<br>';
+    } else {
+        echo $i . '<br>';
+    }
+}
+echo '</div>';
