c09dfb0b608d6f87f188bf6d96e971073d313eac
Josh Harris <49429928+jharris711@users.noreply.github.com>
2024-08-25 20:50:15 -0400

    null coalescing

---
+++ 03-control-structures/09-null-coalescing-operator/index.php
@@ -0,0 +1,17 @@
+<?php
+$favoriteColor = 'red';
+
+# -------------------
+
+# $color = isset($favoriteColor) ? $favoriteColor : 'blue';
+
+# $color = $favoriteColor ?? 'blue';
+
+
+# -------------------
+
+# $color = isset($favoriteColor) ? $favoriteColor : isset($secondFavoriteColor) ? $secondFavoriteColor : 'blue';
+
+$color = $favoriteColor ?? $secondFavoriteColor ?? 'blue';
+
+echo $color;
