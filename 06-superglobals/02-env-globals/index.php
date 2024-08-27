09e8b9e02bc9a07234793c808e59e41a90fcc259
Josh Harris <49429928+jharris711@users.noreply.github.com>
2024-08-26 21:15:59 -0400

    GLOBAL superglobal vars

---
+++ 06-superglobals/02-env-globals/index.php
@@ -0,0 +1,49 @@
+<?php
+
+putenv('DB_HOST=localhost');
+putenv('DB_USER=root');
+
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+
+/* var_dump(getenv()); */
+
+$foo = 'Global variable';
+
+function test() {
+  $bar = 'Local variable';
+  echo 'Local var' . ' ' .  $bar . '<br>';
+  echo 'Global variable inside function: ' . $GLOBALS['foo'];
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>System Information</title>
+  <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="bg-gray-100">
+  <div class="container mx-auto p-8 bg-white shadow-md mt-10 rounded-lg">
+    <h1 class="text-3xl font-semibold mb-4 text-center">System Information</h1>
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+
+      <div class="bg-gray-200 p-4 rounded-lg">
+        <strong class="block mb-2">DB Host: <?= $host ?></strong>
+
+      </div>
+      <div class="bg-gray-200 p-4 rounded-lg">
+        <strong class="block mb-2">DB User: <?= $user ?></strong>
+        <?= test() ?>
+      </div>
+
+    </div>
+</body>
+
+</html>
